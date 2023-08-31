627829b68e9e35e24948fc041e1b0ed46757f860
danil <dantereshenko69@gmail.com>
2023-08-31 15:06:24 +0600

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header__nav'),
+    menuItem = document.querySelectorAll('.header__nav__item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('header_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('header_active');
+        })
+    })
+})
